2940dbb5867be238c62a25c9af9c6fa92d6a5813
Jared Gibson <gibsonjareds@gmail.com>
2021-05-28 15:19:12 -0400

    updates Camera component to allow for multiple steps

---
+++ src/components/Camera/Camera.spec.tsx
@@ -0,0 +1,14 @@
+///<reference types="cypress">
+
+import * as React from "react";
+import {mount} from "@cypress/react";
+import Camera from "./Camera.tsx";
+
+describe("Camera", ()=>{
+    it("passes className properly",()=>{
+        let className = "container";
+        mount(<Camera className={className} />);
+
+        cy.get('[data-role=camera]').should('have.class', className);
+    });
+});
--- src/components/Camera/Camera.tsx
+++ src/components/Camera/Camera.tsx
@@ -1,9 +1,29 @@
 import * as  React from "react";
 import {Button} from "semantic-ui-react";
+import StepOne from "./Steps/StepOne";
 
-export default function Camera() {
-    return <div>
-        <Button>Record from Webcam</Button>
-        <Button>Upload Video File</Button>
-    </div>;
+interface CameraProps {
+    onVideoCreated: (event: object) => any,
+    className : string
 }
+
+interface CameraState {
+    mode:'camera'| 'upload' | 'none',
+    step:'one' | 'two' | 'three' | 'four' | 'five' | 'six'
+}
+
+export default class Camera extends React.Component<CameraProps, CameraState> {
+    state: Readonly<CameraState> = {
+        mode:'none',
+        step:'one'
+    }
+    render(){
+        return <div data-role="camera" className={this.props.className}>
+            <StepOne selectMode={mode =>{  this.setState({mode}); } } />
+        </div>;
+    }
+}
+
+export {
+    StepOne
+};
